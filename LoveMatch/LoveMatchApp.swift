f409abbd9cd571a86e19bfba9ab12c013dce8dfb
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-01-16 14:20:57 -0800

    Initial Commit

---
+++ LoveMatch/LoveMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveMatchApp.swift
+//  LoveMatch
+//
+//  Created by youlserf on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LoveMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
